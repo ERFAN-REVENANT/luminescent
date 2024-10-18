e2a179176669a508231a33234e0678b4acda6b1c
Erfan <mrenmityerf@gmail.com>
2024-10-18 18:23:12 +0330

    scroll looks good

--- src/components/InfiniteScroll.jsx
+++ src/components/InfiniteScroll.jsx
@@ -17,7 +17,7 @@ const InfiniteScroll = () => {
   const infiniteContent = generateInfiniteContent(scrollContent, 20);
 
   return (
-    <div className="px-[30px] bg-customDark py-[50px]">
+    <div className="px-[30px] bg-customDark py-[20px] md:py-[50px]">
       <div className="relative border border-1 border-white rounded-full w-full py-3 flex gap-5 overflow-hidden">
         <div className="scroll-content flex animate-RightToLeft font-modak text-white items-center text-[1.5rem] gap-5 whitespace-nowrap">
           {infiniteContent.map((item, index) => (
